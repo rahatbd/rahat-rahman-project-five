e1e2e5b34a6f14b2b1bdaa21feae08023a725374
Rahat Rahman <63892310+rahatbd@users.noreply.github.com>
2020-08-10 23:58:58 -0400

    Updates

---
+++ precache-manifest.3369d263e5f578e489eb3f25a36601ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e6b4f89e272b65e14c71cd638a1742d",
+    "url": "/rahat-rahman-project-five/index.html"
+  },
+  {
+    "revision": "d2e754ee1f8804969902",
+    "url": "/rahat-rahman-project-five/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e767b4f4da211b52f1e6",
+    "url": "/rahat-rahman-project-five/static/js/2.1bf1ae72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rahat-rahman-project-five/static/js/2.1bf1ae72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2e754ee1f8804969902",
+    "url": "/rahat-rahman-project-five/static/js/main.fc7ccc00.chunk.js"
+  },
+  {
+    "revision": "0cc9ea1e203df42d4644",
+    "url": "/rahat-rahman-project-five/static/js/runtime-main.08c9964d.js"
+  }
+]);
