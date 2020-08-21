2383c528c624ba7ced70125978a4c973f15603df
Rahat Rahman <63892310+rahatbd@users.noreply.github.com>
2020-08-20 21:41:20 -0400

    Updates

---
+++ precache-manifest.c99bfb6f0d0de5ae49bbb13ba675097a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98aa5861a202b56689cd71bb81b2c63f",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "cc243060fe781f55e9d5",
+    "url": "/covid19/static/css/main.293ecfb3.chunk.css"
+  },
+  {
+    "revision": "eb965abab4800662e4ba",
+    "url": "/covid19/static/js/2.dbf7f888.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19/static/js/2.dbf7f888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc243060fe781f55e9d5",
+    "url": "/covid19/static/js/main.a45f563c.chunk.js"
+  },
+  {
+    "revision": "3bcbb459ee18f608419d",
+    "url": "/covid19/static/js/runtime-main.49ccc4aa.js"
+  },
+  {
+    "revision": "ea9dcc33d352fc7e33bbe0b7e7fec559",
+    "url": "/covid19/static/media/marjanblan-unsplash.ea9dcc33.jpg"
+  }
+]);
