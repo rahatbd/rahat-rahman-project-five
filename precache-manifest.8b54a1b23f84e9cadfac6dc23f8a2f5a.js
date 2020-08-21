f46d1f15f22186e8a13e9edf0363c059d7c33ce5
Rahat Rahman <63892310+rahatbd@users.noreply.github.com>
2020-08-21 12:36:48 -0400

    Updates

---
+++ precache-manifest.8b54a1b23f84e9cadfac6dc23f8a2f5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a2f32061e1641d21db3dc1a5ecbfbd3",
+    "url": "/rahat-rahman-project-five/index.html"
+  },
+  {
+    "revision": "577001a473d88812b98f",
+    "url": "/rahat-rahman-project-five/static/css/main.a00682c8.chunk.css"
+  },
+  {
+    "revision": "eb965abab4800662e4ba",
+    "url": "/rahat-rahman-project-five/static/js/2.dbf7f888.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rahat-rahman-project-five/static/js/2.dbf7f888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "577001a473d88812b98f",
+    "url": "/rahat-rahman-project-five/static/js/main.6708f38c.chunk.js"
+  },
+  {
+    "revision": "0cc9ea1e203df42d4644",
+    "url": "/rahat-rahman-project-five/static/js/runtime-main.08c9964d.js"
+  },
+  {
+    "revision": "ea9dcc33d352fc7e33bbe0b7e7fec559",
+    "url": "/rahat-rahman-project-five/static/media/marjanblan-unsplash.ea9dcc33.jpg"
+  }
+]);
